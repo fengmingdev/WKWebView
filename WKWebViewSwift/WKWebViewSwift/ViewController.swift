474c918f17258c1edf55b68e3abdf1af1295ce75
XiaoFeng <xiaofeng@XiaoFengdeMacBook-Pro.local>
2017-01-05 00:53:05 +0800

    完成Swift版WK

--- WKWebViewSwift/WKWebViewSwift/ViewController.swift
+++ WKWebViewSwift/WKWebViewSwift/ViewController.swift
@@ -4,7 +4,8 @@
 //
 //  Created by XiaoFeng on 2017/1/5.
 //  Copyright © 2017年 XiaoFeng. All rights reserved.
-//
+//  QQ群:384089763 欢迎加入
+//  github链接:https://github.com/XFIOSXiaoFeng/WKWebView
 
 import UIKit
 
@@ -15,11 +16,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loadingAction(_ sender: Any) {
+        
+        let web = WKWebViewController()
+        
+        web.loadUrlSting(string: "https://www.baidu.com")
+        
+        navigationController?.pushViewController(web, animated: true)
     }
-
-
 }
 
