e73c68867b9c97a32d11b49466134d25899c9e56
XiaoFeng <xiaofeng@XiaoFengdeMacBook-Pro.local>
2017-01-05 00:18:13 +0800

    初始化Swift项目

---
+++ WKWebViewSwift/WKWebViewSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WKWebViewSwift
+//
+//  Created by XiaoFeng on 2017/1/5.
+//  Copyright © 2017年 XiaoFeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
